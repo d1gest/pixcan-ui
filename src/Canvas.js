9ced0b524568546f127592589bbcbc75025d69e8
dkobylki <dmitrii.kobylkin@t-systems.com>
2019-03-05 18:36:15 +0300

    Canvas (((

--- src/Canvas.js
+++ src/Canvas.js
@@ -16,20 +16,25 @@ class Canvas extends React.Component {
     }
 
     handleClick(e) {
-        let newCoord = [e.pageX, e.pageY];
-        let ctx = this.state.context;
-        ctx.scale(40,40);
+        let scalechange = 3;
+        let offsetX = -(e.nativeEvent.offsetX * scalechange);
+        let offsetY = -(e.nativeEvent.offsetY * scalechange);
+        let newCoord = [offsetX, offsetY];
+        //let newCoord = [e.nativeEvent.offsetX, e.nativeEvent.offsetY];
+        let ctx = this.canvasRef.current.getContext('2d');
+        //ctx.scale(4,4);
+        ctx.fillRect(e.pageX, e.pageY, 15, 15);
         //ctx.translate(e.pageX, e.pageY);
-        ctx.restore();
-        this.setState(state => ({context: ctx}));
-        //this.setState(state => ({isZoomed: !state.isZoomed, coordinates: newCoord}));
+        //ctx.restore();
+        //this.setState(state => ({context: ctx}));
+        this.setState(state => ({isZoomed: !state.isZoomed, coordinates: newCoord}));
         console.log(e.pageX);
     }
 
     handleMove(e) {
         e.preventDefault();
         let newCoord = [e.pageX, e.pageY];
-        this.setState(() => ({coordinates: newCoord}));
+        //this.setState(() => ({coordinates: newCoord}));
     }
 
     componentDidMount() {
@@ -92,20 +97,61 @@ class Canvas extends React.Component {
 
     render() {
         const zoomed = {
-            transform: `translate(${this.state.coordinates[0]},${this.state.coordinates[1]}) scale(40,40)`
+            //transform: `translate(${this.state.coordinates[0]},${this.state.coordinates[1]}) scale(40,40)`
+            transform: `scale(4,4)`
+        };
+        const translate = {
+            transform: `translate(${this.state.coordinates[0]}px,${this.state.coordinates[1]}px)`
+            //transform: 'rotate(20deg)'
         };
         const notZoomed = {};
-        console.log('TEST');
         console.log(zoomed);
 
         return (
-            <div style={this.state.isZoomed ? zoomed : notZoomed}>
-                <canvas ref={this.canvasRef} width={1920} height={1080} onClick={this.handleClick}
-                        onMouseMove={this.handleMove}/>
-
+            <div>
+                <div id='canvdiv' style={this.state.isZoomed ? translate : notZoomed}>
+                    <div style={this.state.isZoomed ? zoomed : notZoomed}>
+                        <canvas ref={this.canvasRef} width={500} height={500} onClick={this.handleClick}
+                                onMouseMove={this.handleMove} onWheel={this.handleWheel}/>
+                    </div>
+                </div>
                 <output>X : {this.state.coordinates[0]} , Y : {this.state.coordinates[1]}</output>
             </div>
         )
+    }
+
+    handleWheel(e) {
+        /*        e.preventDefault();
+                let context = this.canvasRef.current.getContext('2d');
+                // Get mouse offset.
+                var mousex = e.clientX - canvas.offsetLeft;
+                var mousey = e.clientY - canvas.offsetTop;
+                // Normalize wheel to +1 or -1.
+                var wheel = e.deltaY < 0 ? 1 : -1;
+
+                // Compute zoom factor.
+                var zoom = Math.exp(wheel*zoomIntensity);
+
+                // Translate so the visible origin is at the context's origin.
+                context.translate(originx, originy);
+
+                // Compute the new visible origin. Originally the mouse is at a
+                // distance mouse/scale from the corner, we want the point under
+                // the mouse to remain in the same place after the zoom, but this
+                // is at mouse/new_scale away from the corner. Therefore we need to
+                // shift the origin (coordinates of the corner) to account for this.
+                let originx -= mousex/(scale*zoom) - mousex/scale;
+                let originy -= mousey/(scale*zoom) - mousey/scale;
+
+                // Scale it (centered around the origin due to the trasnslate above).
+                context.scale(zoom, zoom);
+                // Offset the visible origin to it's proper position.
+                context.translate(-originx, -originy);
+
+                // Update scale and others.
+        /!*        scale *= zoom;
+                visibleWidth = width / scale;
+                visibleHeight = height / scale;*!/*/
     }
 }
 
